9d0451863d6d86ce3f9652ae39bb57a1fe867f63
NamJiwony778 <71044421+NamJiwony778@users.noreply.github.com>
2020-11-26 17:42:07 -0500

    backend auth

---
+++ backend/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+     secret: 'sportify-secret-key'
+};
---
+++ backend/middlewares/verifySignUp.js
@@ -0,0 +1,41 @@
+const db = require('../models');
+const ROLES = db.ROLES;
+const User = db.user;
+
+checkDuplicateEmail = (req, res, next) => {
+    User.findOne({
+        email: req.body.email
+    }).exec((err, user) => {
+        if(err) {
+            res.status(500).send({ message: err });
+        }
+
+        if(user) {
+            res.status(400).send({ message: 'Échoué! Cet email est déjà utilisé!'});
+            return;
+        }
+
+        next();
+    });
+};
+
+checkRolesExisted = (req, res, next) => {
+   if(req.body.roles) {
+       for (let i = 0; i < req.body.roles.length; i++) {
+           if(!ROLES.includes(req.body.roles[i])) {
+               res.status(400).send({
+                  message: `Échoué! Role ${req.body.roles[i]} n'est pas existé!`
+               });
+               return;
+           }
+       }
+   }
+   next();
+};
+
+const verifySignUp = {
+      checkDuplicateEmail,
+      checkRolesExisted
+};
+
+module.exports = verifySignUp;
